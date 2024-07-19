6a7a7253f339dd56ba6dbdc0026f7e9135caf7d6
nada.daadi@esprit.tn <nada.daadi@esprit.tn>
2024-07-19 09:16:34 +0100

    modif

--- home.php
+++ home.php
@@ -17,11 +17,12 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Home</title>
 </head>
 <body>
-    <div class="container">
-    <nav class="navbar bg-body-tertiary fixed-top">
+<div class="container">
+<nav class="navbar bg-body-tertiary fixed-top">
         <div class="container-fluid">
            <img src="image/logo.png" alt="logo" class="logo">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -29,46 +30,32 @@ if (isset($_SESSION['username'])) {
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><?php echo htmlspecialchars($username); ?></h5>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="home.php"><i class="fas fa-home"></i> Home</a>
 
                         </li>
                         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Products
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="products.php">Product List</a></li>
-              <li><a class="dropdown-item" href="scrap.php">Scrap List</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="product.php">Products Catalog</a></li>
-            </ul>
-          </li>
-        
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-box"></i> Products</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="products.php">Product List</a></li>
+                                <li><a class="dropdown-item" href="scrap.php">Scrap List</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="product.php">Products Catalog</a></li>
+                            </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.php">contact</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="settings.php">User Settings</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>     
-</ul>
+                        <li class="nav-item"><a class="nav-link" href="about.php"><i class="fas fa-info-circle"></i> About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.php"><i class="fas fa-envelope"></i> contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="settings.php"><i class="fas fa-cog"></i> User Settings</a></li>
+                        <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>     
+                    </ul>
                     <form class="d-flex mt-3" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn btn-outline-success" type="submit"><i class="fas fa-search"></i> Search</button>
                     </form>
                 </div>
             </div>
--- scrap.php
+++ scrap.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['username'])) {
 </head>
 <body>
 <div class="container">
-<nav class="navbar bg-body-tertiary fixed-top">
+    <nav class="navbar bg-body-tertiary fixed-top">
         <div class="container-fluid">
            <img src="image/logo.png" alt="logo" class="logo">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -47,47 +47,36 @@ if (isset($_SESSION['username'])) {
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><?php echo htmlspecialchars($username); ?></</h5>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                            <a class="nav-link active" aria-current="page" href="home.php"><i class="fas fa-home"></i> Home</a>
 
                         </li>
                         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Products
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="products.php">Product List</a></li>
-              <li><a class="dropdown-item" href="scrap.php">Scrap List</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="product.php">Products Catalog</a></li>
-            </ul>
-          </li>
-        
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-box"></i> Products</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="products.php">Product List</a></li>
+                                <li><a class="dropdown-item" href="scrap.php">Scrap List</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="product.php">Products Catalog</a></li>
+                            </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.php">contact</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="settings.php">User Settings</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>     
-</ul>
-                        <form method="GET" action="" class="d-flex mt-3">
-        <input type="search" name="search" class="form-control me-2"  placeholder="Search..." value="<?php echo $search; ?>" required>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <li class="nav-item"><a class="nav-link" href="about.php"><i class="fas fa-info-circle"></i> About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.php"><i class="fas fa-envelope"></i> contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="settings.php"><i class="fas fa-cog"></i> User Settings</a></li>
+                        <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>     
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit"><i class="fas fa-search"></i> Search</button>
                     </form>
+                </div>
+            </div>
+        </div>
     </nav>
     <span class='result-count'><?php echo $result->num_rows; ?> product(s) found</span>
     <h1>Scrap List</h1>
